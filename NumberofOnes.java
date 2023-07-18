3e0c53536dfe170ac7f0420af3ba4059f82d69a1
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-18 12:34:13 +0530

    no of 1s in the given num

---
+++ NumberofOnes.java
@@ -0,0 +1,23 @@
+package NumofOnes;
+import java.util.Scanner;
+
+public class NumberofOnes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc .nextInt();
+        System.out.println(numone(num));
+    }
+    public static int numone(int a)
+    {
+        int val = 0;
+        while(a!=0)
+        {
+            if((a&1)==1)
+            {
+                val++;
+            }
+            a = a >> 1;
+        }
+        return val;
+    }
+}
